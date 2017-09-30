742f827916fe442c68814b2ed3075007ac263cae
mbobkova <masha.bobkova997@gmail.com>
2017-09-30 19:16:34 +0300

    finite-state-machine: implement a part of methods

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
-
+    constructor(config) {
+        this.initial = config.initial;   
+        this.state = this.initial; 
+        this.states = config.states;
+        this.historyStates = [];
+    }
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) { 
+       /* if(state == 'normal' || state == 'busy' || state == 'hungry' || state == 'sleeping'){
+            this.state = state;
+        }  else throw new Error("Not a correct state");
+        if(this.states.hasOwnProperty(state)){
+            this.state = state;
+            this.historyStates.push(state);
+        }
+        else throw new Error("Not a correct state");*/
+        var arr = Object.keys(this.states);
+        for(var i=0; i<arr.length; i++){
+            if(arr[i] == state){
+                this.state = state;
+                return;
+            }
+        }
+        throw new Error("Not a correct state");
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        for(var prop in this.states[this.state].transitions){
+            if(prop == event){
+                this.state = this.states[this.state].transitions[event];
+                return;
+            }
+        }
+        throw new Error("Not a correct state");
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,7 +67,21 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        var arr = [];
+
+        if(event){
+            for(var prop in this.states){
+                if(this.states[prop].transitions.hasOwnProperty(event)){
+                    arr.push(prop);     
+                }
+            }
+            return arr;
+        } 
+        else if(!event){
+            return Object.keys(this.states);
+        }
+    }
 
     /**
      * Goes back to previous state.
